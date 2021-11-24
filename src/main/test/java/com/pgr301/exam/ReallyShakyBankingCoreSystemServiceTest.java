b4698886d62d49d1f05f69c3748b9bd482f3ffd6
WhoCanThisBe <wen.hhp@gmail.com>
2021-11-24 14:13:27 +0100

    Initial commit

---
+++ src/main/test/java/com/pgr301/exam/ReallyShakyBankingCoreSystemServiceTest.java
@@ -0,0 +1,16 @@
+package com.pgr301.exam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReallyShakyBankingCoreSystemServiceTest {
+
+
+    @Test
+    void stupidTest() {
+        String stringTest =  "test";
+        assertEquals(stringTest,"test1");
+    }
+
+}
