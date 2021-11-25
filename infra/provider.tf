fa89505e0fcc0d5f093cdbf06242a9503085867a
WhoCanThisBe <wen.hhp@gmail.com>
2021-11-25 19:58:40 +0100

    try to run terraform in correct folder test 7

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,7 +7,8 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-wehu001-terraform"
-    key    = "pgr301-wehu001-terraform.state"
+    key    = "terraform.state"
     region = "eu-west-1"
   }
+
 }
