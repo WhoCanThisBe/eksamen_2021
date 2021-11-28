f5d12920fc40d714f9a993a68be3684db315da9e
WhoCanThisBe <wen.hhp@gmail.com>
2021-11-28 03:14:23 +0100

    trying something new on time metrics

--- src/main/java/com/pgr301/exam/BankAccountController.java
+++ src/main/java/com/pgr301/exam/BankAccountController.java
@@ -2,20 +2,18 @@ package com.pgr301.exam;
 
 import com.pgr301.exam.model.Account;
 import com.pgr301.exam.model.Transaction;
+
 import io.micrometer.core.annotation.Timed;
-import io.micrometer.core.aop.TimedAspect;
-import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Metrics;
+
+import io.micrometer.core.instrument.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static java.math.BigDecimal.*;
@@ -28,39 +26,42 @@ public class BankAccountController implements ApplicationListener<ApplicationRea
     private BankingCoreSystmeService bankService;
 
 
-    @Autowired
-    private MeterRegistry meterRegistry;
-
-    @Autowired
-    public BankAccountController(MeterRegistry meterRegistry) {
-        this.meterRegistry = meterRegistry;
-    }
 
 
     @PostMapping(path = "/account/{fromAccount}/transfer/{toAccount}", consumes = "application/json", produces = "application/json")
     public void transfer(@RequestBody Transaction tx, @PathVariable String fromAccount, @PathVariable String toAccount) {
         long start = System.currentTimeMillis();
         bankService.transfer(tx, fromAccount, toAccount);
-        meterRegistry.timer("post_transfer", "duration", String.valueOf(valueOf(System.currentTimeMillis()-start))).record(System.currentTimeMillis()-start, TimeUnit.MILLISECONDS);
+        Metrics.timer("post_transfer", "duration", String.valueOf(valueOf(System.currentTimeMillis()-start))).record(System.currentTimeMillis()-start, TimeUnit.MILLISECONDS);
 
     }
-
+    @Timed("Pal2")
     @PostMapping(path = "/account", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Account> updateAccount(@RequestBody Account a) {
         long start = System.currentTimeMillis();
         bankService.updateAccount(a);
-        meterRegistry.timer("postAccount", "duration", String.valueOf(valueOf(System.currentTimeMillis()-start))).record(System.currentTimeMillis()-start, TimeUnit.MILLISECONDS);
+        Metrics.timer("postAccount", "duration", String.valueOf(valueOf(System.currentTimeMillis()-start))).record(System.currentTimeMillis()-start, TimeUnit.MILLISECONDS);
         return new ResponseEntity<>(a, HttpStatus.OK);
     }
 
-
+    @Timed("pal")
     @GetMapping(path = "/account/{accountId}", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Account> balance(@PathVariable String accountId) {
         long start = System.currentTimeMillis();
-        meterRegistry.timer("getAccount", "duration", String.valueOf(valueOf(System.currentTimeMillis()-start))).record(System.currentTimeMillis()-start, TimeUnit.MILLISECONDS);
         Account account = ofNullable(bankService.getAccount(accountId)).orElseThrow(AccountNotFoundException::new);
-
+        Metrics.timer("getAccountas", "duration", String.valueOf(valueOf(System.currentTimeMillis()-start))).record(System.currentTimeMillis()-start, TimeUnit.MILLISECONDS);
         return new ResponseEntity<>(account, HttpStatus.OK);
+
+//
+//        try{
+//            Account account = ofNullable(bankService.getAccount(accountId)).orElseThrow(AccountNotFoundException::new);
+//            Metrics.timer("getAccount", "duration", String.valueOf(valueOf(System.currentTimeMillis()-start))).record(System.currentTimeMillis()-start, TimeUnit.MILLISECONDS);
+//            return new ResponseEntity<>(account, HttpStatus.OK);
+//        }catch (BackEndException b){
+//            Metrics.timer("getAccount", "duration", String.valueOf(valueOf(System.currentTimeMillis()-start))).record(System.currentTimeMillis()-start, TimeUnit.MILLISECONDS);
+//            throw new BackEndException();
+//        }
+
     }
 
     @Override
