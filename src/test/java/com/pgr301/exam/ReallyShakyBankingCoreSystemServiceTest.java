a8e6b1d461e5ec77356b184e5d6e9fcbd9d98ecb
WhoCanThisBe <wen.hhp@gmail.com>
2021-11-30 16:00:44 +0100

    test

--- src/test/java/com/pgr301/exam/ReallyShakyBankingCoreSystemServiceTest.java
+++ src/test/java/com/pgr301/exam/ReallyShakyBankingCoreSystemServiceTest.java
@@ -9,8 +9,8 @@ class ReallyShakyBankingCoreSystemServiceTest {
 
     @Test
     void stupidTest() {
-        String stringTest =  "test121";
-        assertEquals(stringTest,"test121");
+        String stringTest =  "test1211";
+        assertEquals(stringTest,"test1211");
     }
 
 }
