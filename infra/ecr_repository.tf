6f37fc473fae953f6138a941f4cd7ad7f7902090
glennbech <glenn.bech@gmail.com>
2021-12-21 20:46:43 +0100

    extr repo

--- infra/ecr_repository.tf
+++ infra/ecr_repository.tf
@@ -5,3 +5,11 @@ resource "aws_ecr_repository" "repository" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "repository" {
+  name = "wehu001-sensur"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
